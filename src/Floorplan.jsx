a8e303b33471b98af2156849ee535562aeddd346
Haziq Naeem <haziqnaeem36@gmail.com>
2022-03-14 18:58:26 -0400

    add bedNum {}

--- src/Floorplan.jsx
+++ src/Floorplan.jsx
@@ -10,9 +10,9 @@ const Floorplan = () => {
     <div>
       <Kitchen/>
       <LivingRoom/>
-      <Bedroom bedNum = '1' />
-      <Bedroom bedNum = '2' />
-      <Bedroom bedNum = '3' />
+      <Bedroom bedNum = {1} />
+      <Bedroom bedNum = {2} />
+      <Bedroom bedNum = {3} />
       <Bath size = 'Half' />
       <Bath size = 'Full' />
     </div>
